bcba78b5ace12aa65817add627eb1df50a0ba048
Ankit Mathur <amathur@athenahealth.com>
2021-10-03 23:47:38 -0400

    check in class for players

---
+++ src/players.py
@@ -0,0 +1,6 @@
+class Player():
+    """A class to define the behaviour of each of the 4 players playing the game.
+    """
+    def __init__(self, uid: int, dealer: bool = False) -> None:
+        uid = uid
+        dealer = dealer
