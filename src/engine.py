0806140b3277ef7d6fcb069d3929881369ac9625
Ankit Mathur <amathur@athenahealth.com>
2021-10-04 00:09:14 -0400

    init game engine

---
+++ src/engine.py
@@ -0,0 +1,26 @@
+"""Main file to test run the game.
+"""
+NUM_OF_MATCHES = 1
+
+def main():
+    """
+    Begin series:
+        Define stopping condition (num of matches)
+        Initialize players
+        Initialize scoreboard
+    Begin match:
+        Identify dealer
+        Shuffle and deal 4 cards
+        Identify kholu and color of trump
+        Deal remaining 4 cards
+    Begin hand:
+        Identify color for the hand
+        Identify active player
+        ...
+    """
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
