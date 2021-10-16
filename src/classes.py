7e1920faa6affcd9805a5b6fe56fddabc61333e0
Nitesh Singh Jaswal <njaswal@pop-os.localdomain>
2021-10-16 14:25:24 +0530

    Enums defined. Game logic not yet implemented.

--- src/classes.py
+++ src/classes.py
@@ -1,40 +1,101 @@
 import random
 import constants
+import enum
+import uuid
+@enum.unique
+class Suit(enum.Enum):
+    HEARTS = 1
+    SPADES = 2
+    DIAMONDS = 3
+    CLUBS = 4
 
+
+@enum.unique
+class CardType(enum.IntEnum):
+    JACK = 8
+    NINE = 7
+    ACE = 6
+    TEN = 5
+    KING = 4
+    QUEEN = 3
+    EIGHT = 2
+    SEVEN = 1
+
+    def points(self) -> int:
+        if self.name == "JACK":
+            return 3
+        elif self.name == "NINE":
+            return 2
+        elif self.name == "ACE" or self.name == "TEN":
+            return 1
+        else:
+            return 0
+
+class TrumpType(enum.Enum):
+    
+    NORMAL_HEARTS = ("NORMAL", Suit.HEARTS)
+    NORMAL_SPADES = ("NORMAL", Suit.SPADES)
+    NORMAL_DIAMONDS = ("NORMAL", Suit.DIAMONDS)
+    NORMAL_CLUBS = ("NORMAL", Suit.CLUBS)
+    REVERSE_HEARTS = ("REVERSE", Suit.HEARTS)
+    REVERSE_SPADES = ("REVERSE", Suit.SPADES)
+    REVERSE_DIAMONDS = ("REVERSE", Suit.DIAMONDS)
+    REVERSE_CLUBS = ("REVERSE", Suit.CLUBS)
+    NO_TRUMP = "NO_TRUMP"
+    REVERSE_NO_TRUMP = "REVERSE_NO_TRUMP"
+
+    def __init__(self, value, suit=None):
+        self.suit = suit
+        self.type = value
+    
 class Card:
     """Class for each card in the deck.
     """
-    def __init__(self, suit: str, value: 'str', points: int, order: int) -> None:
+    def __init__(self, suit: Suit, card_type: CardType):
         """Constructor for the class Card.
 
         Args:
-            suit (str): Suit of the card.
-            value (str): Value of the card.
-            points (int): Number of points the card is worth.
-            order (int): Rank of the card as compared to other cards.
+            suit (Suit): Suit of the card. Represented by Enum
+            card_type (CardType): Type of the Card. Also encodes its points. Represented by IntEnum
         """
         self.suit = suit
-        self.value = value
-        self.points = points
-        self.order = order
+        self.card_type = card_type
         
     def show(self):
-        print("{} of {}".format(self.value, self.suit))
+        print("{} of {}".format(self.card_type.name, self.suit.name))
 
+    def key(self):
+        return f"{self.suit.name}_{self.card_type.name}"
+
+    # def __lt__(self, other):
+    #     if self.suit == other.suit:
+    #         pass
+
+    def __eq__(self, other) -> bool:
+        if self.suit == other.suit and self.card_type == other.card_type:
+            return True
+        return False
 
 class Deck:
-    """Class for the deck of cards.
+    """ Class for the deck of cards.
+       
+        Attrs:
+            cards (list): A list of Card objects.
     """
-    def __init__(self, shuffle: bool = False) -> list:
-        """Constructor method for building a deck of cards.
+    def __init__(self, shuffle: bool = True):
+        """Constructor method for building a deck of cards. 
 
         Args:
-            shuffle (bool, optional): Whether to shuffle the cards or not. Defaults to False.
+            shuffle (bool, optional): Whether to shuffle the cards or not. Defaults to True.
 
-        Returns:
-            list: Ordered deck of cards.
         """
-        self.cards = list(constants.CARDS)
+        cards = []
+        for _, s in Suit.__members__.items():
+            for _, ct in CardType.__members__.items():
+                card = Card(suit=s, card_type=ct)
+                cards.append(card)
+
+        self.cards = cards 
         if shuffle:
             random.shuffle(self.cards)
     
@@ -54,8 +115,12 @@ class Player():
             team (int): Unique key for player's team.
             name (str): Name of the player.
         """
-        self.team = team
+        # Is it necessary to store information of the team with player object?
+        # self.team = team
         self.name = name
+        self.id = uuid.uuid4()
+        # Should we rename this? Because according to our readme, a hand is 1 round of 4 cards
+        # played by all 4 players with determined winner of the hand
         self.hand = []
 
     def draw(self, deck):
